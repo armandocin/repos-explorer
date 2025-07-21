9720674cf0e0fc928fdf313d45b900c73f591dfb
armandocin <armando@rebrandly.com>
2025-07-21 13:14:02 +0200

    conditional vite base path. fixes yarn dev command

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
-export default defineConfig({
-  plugins: [
-    react(),
-    svgr()
-  ],
-  base: '/repos-explorer',
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
+export default defineConfig(({ command }) => {
+  const base = command === 'build' ? '/repos-explorer/' : '/'
+
+  return {
+    plugins: [
+      react(),
+      svgr()
+    ],
+    base: base,
+    build: {
+      outDir: 'dist',
+      sourcemap: true,
+    }
   }
 })
